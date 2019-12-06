a3d15f0ab5e6d00430f0eda9ecab300e6c957adf
Edward Stoner <edwardlstoner@gmail.com>
2019-12-06 10:13:55 -0700

    Eliminate repeat answers

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ var happyResponses = ["You go!", "Your smile just cheered me up!", "YAY!"];
 var sillyResponses = ["Interesting response", "Sounds ... good?", "Me too."];
 var cryingResponses = ["Do you want to talk?", "Keep your head up", "We can cry together!","What can I do?"]
 
-happyEmoji.addEventListener('click', showResponse);
+happyEmoji.addEventListener('click', showResponseHappy);
+sillyEmoji.addEventListener('click', showResponseSilly);
+cryingEmoji.addEventListener('click', showResponseCrying);
 
-function showResponse () {
+function showResponseHappy () {
   var index = Math.floor(Math.random() * 3);
   var msg = document.querySelector('.message');
-  msg.innerText = happyResponses[index];
+  if (msg.innerText == happyResponses[index]) {
+    index = Math.floor(Math.random() * 3);
+    showResponseHappy();
+  } else {
+    msg.innerText = happyResponses[index];
+  }
+}
+
+function showResponseSilly () {
+  var index = Math.floor(Math.random() * 3);
+  var msg = document.querySelector('.message');
+  if (msg.innerText == sillyResponses[index]) {
+    index = Math.floor(Math.random() * 3);
+    showResponseSilly();
+  } else {
+    msg.innerText = sillyResponses[index];
+  }
+}
+
+function showResponseCrying () {
+  var index = Math.floor(Math.random() * 3);
+  var msg = document.querySelector('.message');
+  if (msg.innerText == cryingResponses[index]) {
+    index = Math.floor(Math.random() * 3);
+    showResponseCrying();
+  } else {
+    msg.innerText = cryingResponses[index];
+  }
 }
