8b7a7e1a3695bafd6a4a25e121faef48bda14e77
Edward Stoner <edwardlstoner@gmail.com>
2019-12-06 10:27:42 -0700

    Add semicolon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var sillyEmoji = document.querySelector('.silly');
 var cryingEmoji = document.querySelector('.crying');
 var happyResponses = ["You go!", "Your smile just cheered me up!", "YAY!"];
 var sillyResponses = ["Interesting response", "Sounds ... good?", "Me too."];
-var cryingResponses = ["Do you want to talk?", "Keep your head up", "We can cry together!","What can I do?"]
+var cryingResponses = ["Do you want to talk?", "Keep your head up", "We can cry together!","What can I do?"];
 
 happyEmoji.addEventListener('click', showResponseHappy);
 sillyEmoji.addEventListener('click', showResponseSilly);
